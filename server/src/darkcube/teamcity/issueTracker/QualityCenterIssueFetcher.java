9b6d1ebf9c7832f9b85cc38a6e01e2af9b117be3
Timothy Lusk <darkcube@gmail.com>
2013-08-02 03:23:27 -0400

    Integrate QualityCenter-Java-Client library into the plugin. Plugin now returns actual defect data!

--- server/src/darkcube/teamcity/issueTracker/QualityCenterIssueFetcher.java
+++ server/src/darkcube/teamcity/issueTracker/QualityCenterIssueFetcher.java
@@ -1,12 +1,19 @@
 package darkcube.teamcity.issueTracker;
 
-import jetbrains.buildServer.issueTracker.errors.NotFoundException;
+import darkcube.qc.client.QCRestClient;
+import darkcube.qc.model.Defect;
+import darkcube.qc.model.DefectField;
+import darkcube.qc.model.Domain;
+import darkcube.qc.model.Project;
 import jetbrains.buildServer.util.cache.EhCacheUtil;
 import jetbrains.buildServer.issueTracker.AbstractIssueFetcher;
 import jetbrains.buildServer.issueTracker.IssueData;
 import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class QualityCenterIssueFetcher extends AbstractIssueFetcher {
 
@@ -17,7 +24,23 @@ public class QualityCenterIssueFetcher extends AbstractIssueFetcher {
   @NotNull
   @Override
   public IssueData getIssue(@NotNull String host, @NotNull String id, @Nullable Credentials credentials) throws Exception {
-    throw new NotFoundException("UNIMPLEMENTED");
+    String username = "";
+    String password = "";
+
+    if(credentials instanceof UsernamePasswordCredentials) {
+      username = ((UsernamePasswordCredentials)credentials).getUserName();
+      password = ((UsernamePasswordCredentials)credentials).getPassword();
+    }
+
+    QCRestClient client = new QCRestClient(host, username, password);
+    client.login();
+
+    List<Domain> domains = client.getDomains();
+    List<Project> projects = client.getProjects(domains.get(0).getName());
+    Defect defect = client.getDefect(domains.get(0).getName(),projects.get(0).getName(),Integer.parseInt(id));
+
+    IssueData issueData = new IssueData(id, defect.getField(DefectField.NAME), defect.getField(DefectField.STATUS), getUrl(host, id), false);
+    return issueData;
   }
 
   @NotNull
